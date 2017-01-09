c7ea849da671a7e3cfd09403d2f9ef23ca1e1aeb
Eliot Baker <eliotbaker@gmail.com>
2017-01-09 01:36:13 -0800

    AboutPage added

--- src/pages/login/page.js
+++ src/pages/login/page.js
@@ -8,12 +8,17 @@ export default class LoginPage extends React.Component {
     browserHistory.push('/home');
   }
   
+  goAbout() {
+    browserHistory.push('/about');
+  }
+  
   render() {
     return (
       <div className={styles.content}>
         <h1 className={styles.heading}>Login Page</h1>
         <p className={styles.lead}>Create an account to get started!</p>
         <button className={styles.signUpButton} onClick={this.signUp}>Sign up</button>
+        <button className={styles.signUpButton} onClick={this.goAbout}>About</button>
       </div>
     );
   }
